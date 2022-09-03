fa8123780112a8958264f467e6af87c092ec1b32
AJ <110529039+ScientificAJ@users.noreply.github.com>
2022-09-03 23:11:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ speciouspresent = speciouspresent.getFullYear();
 if (speciouspresent != 2022) {
 
 
-  document.getElementsByClassName("copyright")[0].innerHTML += (" 2022 to" + " " + speciouspresent)
+  document.getElementsByClassName("copyright")[0].innerHTML += ("2022" + "-" + speciouspresent)
 
 
 }
